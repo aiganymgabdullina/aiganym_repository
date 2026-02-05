ad9b463031ce47d82ea3ddda6a6074b9c1af41c6
Aiganym <aiganymgabdullina2007@gmail.com>
2026-02-05 09:12:01 +0500

    second commit

--- labaratory1.py
+++ labaratory1.py
@@ -1,30 +1,48 @@
-#1
+#1 esep
 def analyze_text(text):
     text1 = text.lower()
     text2 = ""
     for alpha in text1:
         if alpha.isalpha() or alpha == " ":
             text2 += alpha
-    dauysty = "aeiouy"git
+    dauysty = "aeiouy"
     zhana_text = ""
     for alpha in text2:
         if alpha in dauysty:
             zhana_text += alpha
     text3 = text2.split()
-    result = ""
+    jauap = ""
     text4 = ''
     for word in text3:
         if len(word) >= 5:
             if word[0] == word[-1]:
-                if word not in result:
-                    result += word + " "
+                if word not in jauap:
+                    jauap += word + " "
                     text4 += word + "," + " "
 
     return {
         "дауысты әріптер:" : set(zhana_text),
     "дауысты әріптер саны:": len(set(zhana_text)),
         "слова длиной ≥ 5:": text4,
-        "жауабы:": result
+        "жауабы:":jauap
     }
-kerek_matyn = input("керек мәтінді жазыңыз:")
+kerek_matyn = input("1 esep : мәтін:")
 print(analyze_text(kerek_matyn))
+
+
+
+#2 esep
+process = lambda s: " ".join(words := list(filter(
+    lambda word: len(word) % 2 == 0,
+    reversed_w := list(map(
+        lambda word: word[::-1],
+        no_digits := list(filter(
+            lambda word: all(not ch.isdigit() for ch in word),
+            s.split()
+        ))
+    ))
+)))
+
+# Проверка
+text = input("2 esep: мәтін: ")
+print(process(text))
