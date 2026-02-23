8229d9751ad27c7b5bfc6fe07668f71075141c76
Aiganym <aiganymgabdullina2007@gmail.com>
2026-02-23 14:59:31 +0500

    lab2 changes

--- lab2/labaratory2.py
+++ lab2/labaratory2.py
@@ -1,25 +1,4 @@
-
-file = open("shop_logs.txt", "r")
-unique_users = set()
-total_buys = 0
-total_sum = 0
-user_spending = {}
-for line in file:
-    parts = line.strip().split(";")
-
-user_id = parts[1]
-
-action = parts[2]
-unique_users.add(user_id)
-if action == "BUY":
-    total_buys += 1
-    amount = int(parts[3])
-    total_sum += amount
-    if user_id not in user_spending:
-        user_spending[user_id] = amount
-    else:
-        user_spending[user_id] += amount
-file.close()
+#1 esep
 f=open("shop_logs.txt","r")
 unique_users= set()
 total_buys=0
@@ -45,6 +24,7 @@ for line in f:
             user_spending[user_id] = n
         else:
             user_spending[user_id] += n
+
 f.close()
 max_user= ""
 max_spend=0
@@ -54,14 +34,17 @@ for user in user_spending:
         max_spend=user_spending[user]
         max_user=user
 if total_buys >0:
-    u=total_sum / total_buys
+    chek=total_sum / total_buys
 else:
-    u=0
+    chek=0
 report = open("report.txt","w",encoding="utf-8")
 report.write("Уникальных пользователей:"+ str(len(unique_users))+"\n")
 report.write("Всего покупок:"+ str(total_buys)+"\n")
 report.write("Общая сумма:"+ str(total_sum)+"\n")
 report.write("Самый активный покупатель:"+max_user+"\n")
-report.write("Средний чек:"+ str(u) +"\n")
+report.write("Средний чек:"+ str(chek) +"\n")
 report.close()
 print("Отчет успешно создан!")
+
+
+#2 esep
