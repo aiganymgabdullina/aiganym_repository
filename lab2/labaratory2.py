376171ea16dab160b8e9e1ba22862d7af5559a74
Aiganym <aiganymgabdullina2007@gmail.com>
2026-02-23 16:33:13 +0500

    lab2 2 esep

--- lab2/labaratory2.py
+++ lab2/labaratory2.py
@@ -1,4 +1,5 @@
 #1 esep
+print("1 esep")
 f=open("shop_logs.txt","r")
 unique_users= set()
 total_buys=0
@@ -48,3 +49,43 @@ print("Отчет успешно создан!")
 
 
 #2 esep
+print("2 esep")
+import csv
+employees = []
+departments = {}
+total_salary = 0
+with open("employees.csv", "r") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        name = row["name"]
+        department = row["department"]
+        salary = int(row["salary"])
+        employee = {"name": name, "department": department,"salary": salary}
+        employees.append(employee)
+        total_salary += salary
+        if department not in departments:
+            departments[department] = []
+        departments[department].append(employee)
+
+average_salary = total_salary/len(employees)
+
+department_averages ={}
+for d in departments:
+    d_total = 0
+    for e in departments[d]:
+        d_total += e["salary"]
+    d_average = d_total /len(departments[d])
+    department_averages[d] = d_average
+
+highest_department = max(department_averages, key = department_averages.get)
+highest_emp = max(employees, key = lambda x: x["salary"])
+hight_salary_employees = []
+for emp in employees:
+    if emp["salary"] > average_salary:
+        hight_salary_employees.append(emp)
+
+with open("hight_salary.csv", "w") as f:
+    a = csv.DictWriter(f, fieldnames=["name", "department", "salary"])
+    a.writeheader()
+    a.writerows(hight_salary_employees)
+
