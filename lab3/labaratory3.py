15a346e7667551d3611d389eaad79c96564c9c33
Aiganym <aiganymgabdullina2007@gmail.com>
2026-02-28 15:24:20 +0500

    esep2 lab3

--- lab3/labaratory3.py
+++ lab3/labaratory3.py
@@ -3,3 +3,9 @@ chek = lambda x: "положительное" if x> 0 else ("ноль" if x == 0
 print(chek(-5))
 print(chek(0))
 print(chek(2))
+
+#ЗАДАЧА 2
+words = ["арбуз", "кот", "машина", "дом", "ананас"]
+words_sorted = sorted(words, key = lambda word: (len(word), word[0]))
+print(words_sorted)
+
