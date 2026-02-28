84f317bd6faf0be0fe5b4b90136e6b06caf8d68b
Aiganym <aiganymgabdullina2007@gmail.com>
2026-02-28 15:32:42 +0500

    3 esep lab3

--- lab3/labaratory3.py
+++ lab3/labaratory3.py
@@ -9,3 +9,7 @@ words = ["арбуз", "кот", "машина", "дом", "ананас"]
 words_sorted = sorted(words, key = lambda word: (len(word), word[0]))
 print(words_sorted)
 
+#ЗАДАЧА 3
+numbers = [5, 12, 7, 20, 33, 8]
+numbers_filtered = list(filter(lambda x: x%2 ==0 and x>10 , numbers))
+print(numbers_filtered)
