060ce43eb3435b4554d50a39bb46fe98650ead83
Aiganym <aiganymgabdullina2007@gmail.com>
2026-03-04 18:32:33 +0500

    Задачи для понимания map and filter 4 esep

--- lab3/labaratory3.py
+++ lab3/labaratory3.py
@@ -202,3 +202,8 @@ print(words_map)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers_new = list(filter(lambda n : n%2 == 0, numbers))
 print(numbers_new)
+
+#ЗАДАЧА 4
+numbers = [0, 5, 12, 7, 20, -3, 8]
+numbers_map = list(map(lambda n: n/2 if n%2==0 else n*3, filter(lambda n: n>5 , numbers)))
+print(numbers_map)
