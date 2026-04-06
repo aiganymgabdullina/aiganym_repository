f6fa6eccb084e156e9aeff986b68e48341aec35e
Aiganym <aiganymgabdullina2007@gmail.com>
2026-04-06 17:15:08 +0500

    lab4 11 esep

--- lab4/lab 4.py
+++ lab4/lab 4.py
@@ -235,5 +235,19 @@ events = [
 iterator = EventIterator(events)
 for e in iterator:
     print(e)
+
+#11 esep
+def damage_stream(events):
+    for event in events:
+        if event.type == "ATTACK":
+            yield event.data.get("damage", 0)
+
+events = [
+    Event("ATTACK", {"damage": 10}),
+    Event("HEAL", {"heal": 5}),
+    Event("ATTACK", {"damage": 20})]
+for dmg in damage_stream(events):
+    print(dmg)
+
 if __name__ == '__main__':
     app.run(port=5001)
