cff38c96a21283a5a3ab88a25791f95bbbe85de5
Aiganym <aiganymgabdullina2007@gmail.com>
2026-04-11 16:16:35 +0500

    lab5 2 esep

--- lab5.py
+++ lab5.py
@@ -6,10 +6,25 @@ class User:
     def __init__(self, user_id: int, name: str, email: str):
         self._id = user_id
         self._name = name.strip().title()
-        email = email.lower()
-        if "@" not in email:
+        clean_email = email.lower().strip()
+        if "@" not in clean_email:
             raise ValueError("Email must contain @")
-        self._email = email
+        self._email = clean_email
+
+#2 esep
+    @classmethod
+    def from_string(cls, data: str):
+        try:
+            parts = data.split(',')
+            if len(parts) != 3:
+                raise ValueError("Строка должна содержать 3 элемента через запятую: id, name, email")
+
+            user_id = int(parts[0].strip())
+            name = parts[1].strip()
+            email = parts[2].strip()
+            return cls(user_id, name, email)
+        except (ValueError, IndexError):
+            raise ValueError("Некорректный формат данных в строке")
 
     def __str__(self):
         return f"User(id={self._id}, name='{self._name}', email='{self._email}')"
@@ -17,20 +32,22 @@ class User:
     def to_dict(self):
         return {"id": self._id, "name": self._name, "email": self._email}
 
-    def __del__(self):
-        print(f"User {self._name} deleted")
-
-
 @app.get("/")
 def home():
     return "Добро пожаловать в магазин!"
-
-
 @app.get("/user/test")
 def test_user():
+    u = User(1, "  john doe  ", "John@Example.COM")
+    return {"data": u.to_dict(), "str_repr": str(u)}
+@app.get("/user/from-string")
+def test_from_string():
     try:
-        u = User(1, " john doe ", "John@Example.COM")
-        return { "as_string": str(u),
-            "as_json": u.to_dict() }
+        raw_data = "2, Alice Wonderland , alice@wonder.com"
+        u = User.from_string(raw_data)
+
+        return {
+            "source_string": raw_data,
+            "created_object": u.to_dict(),
+            "str_repr": str(u) }
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
