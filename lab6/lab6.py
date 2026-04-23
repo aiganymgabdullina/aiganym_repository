17dba431599ed6409f3d2ba8d9906f4eb09b8376
Aiganym <aiganymgabdullina2007@gmail.com>
2026-04-23 16:15:51 +0500

    lab6 2

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -10,3 +10,13 @@ print("\nПервые 5 строк:")
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 print(df.head())
+
+#2 esep
+df = pd.read_excel('catalog_products.xlsx')
+cols_to_fix = df.columns[1:]
+for col in cols_to_fix:
+    df[col] = pd.to_numeric(df[col], errors='coerce').astype(float)
+    df[col] = df[col].fillna(df[col].mean())
+df[cols_to_fix].dtypes.head()
+df[cols_to_fix].isnull().sum().head()
+print(df[['col_2', 'col_3']].head())
