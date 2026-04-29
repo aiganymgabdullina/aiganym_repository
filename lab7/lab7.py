8c9a3d7dcb4e73bfcacd19aa55a9c0941beec26d
Aiganym <aiganymgabdullina2007@gmail.com>
2026-04-29 19:59:21 +0500

    lab7 13

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import PolynomialFeatures
 #1 esep
 file_path = 'catalog_products.xlsx'
 df = pd.read_excel(file_path)
@@ -149,3 +150,17 @@ plt.title('Топ-10 самых важных признаков для пред�
 plt.xlabel('Относительная важность')
 plt.ylabel('Название признака')
 plt.show()
+
+#13 esep
+poly_features = ['col_3', 'total_value', 'log_price']
+poly = PolynomialFeatures(degree=2, include_bias=False)
+X_train_poly = poly.fit_transform(X_train[poly_features])
+X_test_poly = poly.transform(X_test[poly_features])
+model_poly = LinearRegression()
+model_poly.fit(X_train_poly, y_train)
+y_pred_poly = model_poly.predict(X_test_poly)
+mae_poly = mean_absolute_error(y_test, y_pred_poly)
+mse_poly = mean_squared_error(y_test, y_pred_poly)
+print("#13")
+print(f"MAE (Полиномиальная): {mae_poly:.2f}")
+print(f"MSE (Полиномиальная): {mse_poly:.2f}")
