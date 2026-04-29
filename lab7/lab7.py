b22735efeaf5b1466064b18049d835544ade1d6c
Aiganym <aiganymgabdullina2007@gmail.com>
2026-04-29 19:13:01 +0500

    lab7 4

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 #1 esep
 file_path = 'catalog_products.xlsx'
 df = pd.read_excel(file_path)
@@ -25,3 +27,31 @@ df['log_price'] = np.log(df['col_2'])
 df['double_stock'] = df['col_3'] * 2
 print("#3")
 print(df[['col_2', 'col_3', 'total_value', 'log_price', 'double_stock']].head())
+
+#4 esep
+sns.set_theme(style="whitegrid")
+
+
+plt.figure(figsize=(10, 6))
+sns.histplot(df['col_2'], kde=True, color='skyblue')
+plt.title('Распределение цен товаров')
+plt.xlabel('Цена (col_2)')
+plt.ylabel('Частота')
+plt.show()
+
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=df, x='col_3', y='col_2', alpha=0.6)
+plt.title('Связь цены и количества товара')
+plt.xlabel('Количество (col_3)')
+plt.ylabel('Цена (col_2)')
+plt.show()
+
+
+plt.figure(figsize=(12, 8))
+sns.boxplot(data=df, x='col_7', y='col_2')
+plt.title('Разброс цен по категориям')
+plt.xlabel('Категория (col_7)')
+plt.ylabel('Цена (col_2)')
+plt.tight_layout()
+plt.show()
