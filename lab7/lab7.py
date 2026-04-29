28cd8fd2da3daacc4edb7336c44c449da7991c39
Aiganym <aiganymgabdullina2007@gmail.com>
2026-04-29 19:07:24 +0500

    lab7 2

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -6,3 +6,12 @@ print(df.head())
 print(f"\nФорма DataFrame: {df.shape}")
 print(df.dtypes)
 print(df.isnull().sum())
+
+#2 esep
+numeric_cols = df.select_dtypes(include=['number']).columns
+for col in numeric_cols:
+    df[col] = df[col].astype(float).fillna(df[col].mean())
+text_cols = df.select_dtypes(include=['object', 'string']).columns
+df = df.dropna(subset=text_cols)
+print(f"Форма DataFrame после очистки: {df.shape}")
+print(f"Общее количество пропусков: {df.isnull().sum().sum()}")
